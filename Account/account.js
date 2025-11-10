7e25f82b11fd75e3d9f9968afdcabcc89e14aacb
Michael <mw78396@gmail.com>
2025-11-10 11:16:11 -0500

    almost done account HTML and CSS started account JAVASCRIPT

--- Account/account.js
+++ Account/account.js
@@ -1,16 +1,18 @@
-const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
+let users = JSON.parse(localStorage.getItem("users")) || []
+let loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'))
 
 if(!loggedInUser){
-    window.location.href = '/Login-SignUp/login-signUp.html';
-} else {
-    document.getElementById('accountName').textContent = loggedInUser.name;
+    window.location.href = '/Login-SignUp/login-signUp.html'
+} else{
+    document.getElementById('accountName').textContent = loggedInUser.name
+    document.getElementById('accountEmail').textContent = loggedInUser.email || ""
+    if(loggedInUser.profilePic){
+        document.getElementById('profileImage'.src = loggedInUser.profilePic)
+    }
+    loadOrders();
 }
 
-const signOutBtn = document.getElementById('signOutBtn');
-
-function signOut() {
-    localStorage.removeItem('loggedInUser');
-    window.location.href = '/Login-SignUp/login-signUp.html';
-}
-
-signOutBtn.addEventListener('click', signOut);
+document.getElementById('signOutBtn').addEventListener('click', () => {
+    localStorage.removeItem('loggedInUser')
+    window.location.href = '/Login-SignUp/login-signUp.html'
+})
