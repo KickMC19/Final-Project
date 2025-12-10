da5a65b8e7719526f7e2010775a0b556252b069e
CMervy <connerm05@gmail.com>
2025-12-09 21:52:50 -0500

    fix?

--- Account/account.js
+++ Account/account.js
@@ -7,7 +7,7 @@ function formatCurrency(amount) {
 
 // Redirect to login if not logged in
 if (!loggedInUser) {
-    window.location.href = 'https://kickmc19.github.io/Final-Project/Login-SignUp/login-signUp.html';
+    window.location.replace = 'https://kickmc19.github.io/Final-Project/Login-SignUp/login-signUp.html';
 } else {
     document.getElementById('accountName').textContent = loggedInUser.name;
     document.getElementById('accountEmail').textContent = loggedInUser.email || "";
